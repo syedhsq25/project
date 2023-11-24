8cf01294532837a3640462568de6ae3722a9f81b
syedhsq25 <syedhsq25@gmail.com>
2023-11-23 21:00:45 -0800

    11/23/23

---
+++ pattern.js
@@ -0,0 +1,60 @@
+// 1)  1 10
+//     2 9
+//     .,.,,.         
+    
+//     10 1
+//     for(i=1,j=10,k=1;i<=10,j>=1,k<=20;i++,j--,k+=2)
+//     {
+//     console.log(i,j,k)
+    
+// }
+
+    
+
+// 2)
+ //2.1   // 1
+    // 11
+    // 111
+    // 1111
+    // 11111
+
+    // var temp='';
+// for (i=1;i<=5;i++){
+//     temp=temp+'1'
+//     console.log(temp)
+// }
+
+// 2)
+// 2.2) *
+//     **
+//     ***
+//     ****
+
+//     var temp='';
+// // for (i=1;i<=5;i++){
+// //     temp=temp+'*'
+// //     console.log(temp)
+// // // }
+
+// 2.
+// 2.3) when they are strings
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// temp='';
+// for(i=1;i<=5;i++){
+//     temp=temp+i;
+//     console.log(temp)
+// }
+
+// 2 table
+// var a='';
+// for (i=1;i<=10;i++){
+//     console.log('2*',i,'=',2*i);
+// }
+
+//
+week
