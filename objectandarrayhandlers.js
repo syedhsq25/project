b427b46a2a0f33ebe230bccbb437686ec5026a72
syedhsq25 <syedhsq25@gmail.com>
2023-12-27 08:29:39 -0800

    12/27/2023

---
+++ objectandarrayhandlers.js
@@ -0,0 +1,15 @@
+// // const c=
+// // {
+// //     'a':1,
+// //     'b':2,
+// //     'a':2,
+// //     'b':3
+// // }
+// // console.log(Object.keys(c))
+// let a=[1,14,5,2,44,5]
+// console.log(a.sort((a,b)=>a-b))
+// // let s = new Set(a)
+// //console.log(s)
+console.log("hello")
+
+
