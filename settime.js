31398534f6491012b69f0816129d7906ab58e83c
syedhsq25 <syedhsq25@gmail.com>
2023-12-28 23:20:40 -0800

    settimeee

---
+++ settime.js
@@ -0,0 +1,19 @@
+
+for(let i=0;i<=5;i++){
+setTimeout(() => {
+        setTimeout(() => {
+                    
+            for(let j=0;j<=5;j++){
+            console.log(j)
+            }   
+        }, 2000);
+
+
+
+
+
+},  5000);}
+
+setTimeout(() => {
+    console.log("hello again")
+}, 2000);
