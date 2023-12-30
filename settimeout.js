156aa46026148bfd214250ad1c8c6b90ac03a771
syedhsq25 <syedhsq25@gmail.com>
2023-12-29 20:44:09 -0800

    new

--- settimeout.js
+++ settimeout.js
@@ -1,3 +1,8 @@
 setTimeout(() => {
     console.log("maintaining the streak for git!")
 }, 2000);
+setTimeout(() => {
+    console.log("maintaining the streak for git!")
+}, 2000);setTimeout(() => {
+    console.log("maintaining the streak for git!")
+}, 2000);
